a1bacce42482b367089fba6a7d954a1eb6464235
Viktor Pokhvalenyi <mrvikster42@gmail.com>
2018-01-07 16:05:37 +0100

    Step 9 done. Name changing left

--- game.js
+++ game.js
@@ -6,6 +6,14 @@ const playerClasses = {
 let currentPlayer;
 let emptyFields;
 let fields = document.querySelectorAll('.board > div');
+let redDefault = 0;
+let blueDefault = 0;
+let redScore = document.querySelector('#scoreOne');
+let blueScore = document.querySelector('#scoreTwo');
+let reset = document.querySelector('.reset').addEventListener('click', () => {
+	redDefault = redScore.innerHTML = 0;
+	blueDefault = blueScore.innerHTML = 0;
+});
 	initGame();
 	function initGame() {
 		currentPlayer = 'playerA';
@@ -14,11 +22,15 @@ let fields = document.querySelectorAll('.board > div');
 			field.addEventListener('click', fieldClickHandler);
 			field.className = '';
 		});
+		/* reset.addEventListener('click', () => {
+			console.log(reset);
+			redScore.innerHTML = 0;
+			blueScore.innerHTML = 0;
+		}); */
 	};
 	function fieldClickHandler () {
 		let playerClass = playerClasses[currentPlayer];
 		this.classList.add(playerClass);
-		console.log('click', playerClass)
 		currentPlayer === 'playerA' ? currentPlayer = 'playerB' : currentPlayer = 'playerA';
 		this.removeEventListener('click', fieldClickHandler);	
 		emptyFields--;
@@ -37,16 +49,22 @@ let fields = document.querySelectorAll('.board > div');
 		fields[2].className + fields[4].className + fields[6].className
 		]
 		let redWon = winningArray.includes('redredred');
-		let blueWon = winningArray.includes('blueblueblue');			
+		let blueWon = winningArray.includes('blueblueblue');
+
 		if (redWon) {
 			setTimeout(()=> {
 				alert('Player 1 won!');
+				redDefault++;
+				redScore.innerHTML = redDefault;
+				console.log(redDefault)
 				initGame();
 			},100);
 		} 
 		else if (blueWon) {
 			setTimeout(()=> {
 				alert('Player 2 won!');
+				blueDefault++;				
+				blueScore.innerHTML = blueDefault;
 				initGame();
 			},100);
 		} 
