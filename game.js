df8d7aab500465d8924115905f5ca76ed695fc8b
Viktor Pokhvalenyi <mrvikster42@gmail.com>
2018-01-07 12:20:27 +0100

    Step 4 complete

--- game.js
+++ game.js
@@ -1,12 +1,21 @@
+const playerClasses = {
+	playerA: 'red',
+	playerB: 'blue'
+}
+let currentPlayer;
 document.addEventListener('DOMContentLoaded', function () {
 	initGame();
 	function initGame () {
 		let fields = document.querySelectorAll('.board > div');
+		currentPlayer = 'playerA';
 		fields.forEach(field => {
 			field.addEventListener('click', fieldClickHandler);
 		});
 		function fieldClickHandler () {
-			console.log('Hello', this);
+			let playerClass = playerClasses[currentPlayer];
+			this.removeEventListener('click', fieldClickHandler);
+			this.classList.add(playerClass);
+			currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
 		};
 	};
 });
